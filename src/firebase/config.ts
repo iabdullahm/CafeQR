22664f998deb2bcb0414093d7a8aa2f17ce07817
عبدالله الجهوري <abdullah.j@creativetechno.net>
2026-03-17 18:26:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1776869374-8fef9",
+  "appId": "1:42442619017:web:4080128b61598aa67cee58",
+  "apiKey": "AIzaSyBSmNjcxSiJWgMddA4CW6S7xim0Z2IJ9bM",
+  "authDomain": "studio-1776869374-8fef9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "42442619017"
+};
